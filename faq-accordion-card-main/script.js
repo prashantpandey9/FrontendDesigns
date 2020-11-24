6b8e8742e92ce2fd5ace1aada49778c8cf569fcc
prashantpandey9 <prashantpandey94551@gmail.com>
2020-11-24 19:13:10 +0530

    done with this task

---
+++ faq-accordion-card-main/script.js
@@ -0,0 +1,17 @@
+let button = document.querySelectorAll('#button');
+
+let faqAnswer = document.querySelectorAll('.ans');
+
+function toggleActive(target) {
+  let question = target.querySelector('.ques');
+  let answer = target.querySelector('.ans');
+  let showBtn = target.querySelector('.close');
+
+  question.classList.toggle('ques_active');
+  answer.classList.toggle('ans_active');
+  showBtn.classList.toggle('arrow');
+}
+button.forEach(item => {
+    item.addEventListener('click', () => toggleActive(item));
+  });
+
